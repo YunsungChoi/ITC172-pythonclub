f4dc13e475ed9a1a106aebcb4b8d66170aba2dc6
Yunsung Choi <36170628+YunsungChoi@users.noreply.github.com>
2019-05-23 02:11:58 -0700

    Assignment6 Meeting and Detail
    Added Meeting and MeetingDetails html pages and display MeetingTitle, Date, Location, Agenda... etc

--- pythonclub/club/urls.py
+++ pythonclub/club/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns=[
     path('', views.index, name='index'),
     path('gettypes/', views.gettypes, name='resource'),
+    path('getmeetings/', views.getmeetings, name='meeting'),
+    path('meetingdetails/<int:id>', views.meetingdetails, name='meetingdetails'),
 ]
--- pythonclub/club/views.py
+++ pythonclub/club/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Resource
+from .models import Meeting
 
 # Create your views here.
 def index(request): 
@@ -8,3 +9,14 @@ def index(request):
 def gettypes(request):
     type_list=Resource.objects.all()
     return render(request, 'club/resources.html' ,{'type_list' : type_list})
+
+def getmeetings(request):
+    meetings_list=Meeting.objects.all()
+    return render(request, 'club/meetings.html', {'meetings_list': meetings_list})
+
+def meetingdetails(request, id):
+    meet=get_object_or_404(Meeting, pk=id)
+    context={
+        'meet' : meet,
+    }
+    return render(request, 'club/meetingdetails.html', context=context)
